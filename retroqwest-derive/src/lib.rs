f7092197d72c6fe15157130e974588636a0e23b4
Jake Swenson <jake@jakeswenson.com>
2021-09-16 22:18:23 -0700

    feat: Add query param and json body support

--- retroqwest-derive/src/lib.rs
+++ retroqwest-derive/src/lib.rs
@@ -3,90 +3,161 @@ use proc_macro2::Span;
 use quote::{quote, quote_spanned};
 use std::convert::TryFrom;
 use syn::spanned::Spanned;
-use syn::{
-    parse_macro_input, parse_quote, Attribute, FnArg, Ident, ImplItemMethod, ItemTrait, LitStr,
-    Pat, PatType, Signature, TraitItem, TraitItemMethod,
-};
+use syn::{parse_macro_input, parse_quote, Attribute, FnArg, Ident, ImplItemMethod, ItemTrait, LitStr, Pat, PatType, Signature, TraitItem, TraitItemMethod, PatIdent};
 
 #[proc_macro_attribute]
 pub fn retroqwest(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let item = parse_macro_input!(input as ItemTrait);
+  let item = parse_macro_input!(input as ItemTrait);
 
-    expand(item).unwrap_or_else(to_compile_errors).into()
+  expand(item).unwrap_or_else(to_compile_errors).into()
 }
 
 fn to_compile_errors(errors: syn::Error) -> proc_macro2::TokenStream {
-    let compile_errors = errors.to_compile_error();
-    compile_errors
+  let compile_errors = errors.to_compile_error();
+  compile_errors
 }
 
 struct HttpMethodAttribute {
-    method: Ident,
-    _response: Option<Ident>,
-    uri: LitStr,
+  method: Ident,
+  _response: Option<Ident>,
+  uri: LitStr,
 }
 
 impl TryFrom<Attribute> for HttpMethodAttribute {
-    type Error = syn::Error;
-    fn try_from(att: Attribute) -> Result<Self, Self::Error> {
-        let mut segments = att.path.segments.iter();
-        let uri: LitStr = att.parse_args()?;
+  type Error = syn::Error;
+  fn try_from(att: Attribute) -> Result<Self, Self::Error> {
+    let mut segments = att.path.segments.iter();
 
-        Ok(Self {
-            method: segments.next().unwrap().ident.clone(),
-            _response: segments.next().map(|s| s.ident.clone()),
-            uri,
-        })
-    }
+    segments.next().expect("the http segment has to exist");
+
+    let uri: LitStr = att.parse_args()?;
+
+    let path_seg = segments.next().ok_or(syn::Error::new(att.span(), "http attribute missing method"))?;
+
+    Ok(Self {
+      method: path_seg.ident.clone(),
+      _response: None,
+      uri,
+    })
+  }
 }
 
 fn get_att(
-    attrs: &mut Vec<Attribute>,
-    name: &'static str,
-    span: Span,
-) -> Result<Attribute, syn::Error> {
-    let index = attrs
-        .iter()
-        .enumerate()
-        .find_map(move |(i, a)| {
-            a.path
-                .segments
-                .first()
-                .filter(|p| p.ident == name)
-                .map(move |_| i)
-        })
-        .ok_or(syn::Error::new(span, "Missing http method attribute"));
+  attrs: &mut Vec<Attribute>,
+  name: &'static str
+) -> Option<Attribute> {
+  attrs
+    .iter()
+    .enumerate()
+    .find_map(move |(i, a)| {
+      a.path
+        .segments
+        .first()
+        .filter(|p| p.ident == name)
+        .map(move |_| i)
+    }).map(|i| attrs.remove(i))
+}
 
-    index.map(|i| attrs.remove(i))
+enum HttpArg {
+  JsonBody {
+    arg: Ident,
+    span: Span,
+  },
+  Uri {
+    arg: Ident,
+    span: Span,
+  },
+  Query {
+    name: LitStr,
+    arg: Ident,
+    span: Span,
+  },
+}
+
+impl HttpArg {
+  fn parse(arg: &mut FnArg) -> Option<Self> {
+    match arg {
+      FnArg::Typed(PatType { attrs, pat, .. }) => {
+        if let Pat::Ident(PatIdent { ident, .. }) = pat.as_ref() {
+          if let Some(_json_att) = get_att(attrs, "json") {
+            Some(HttpArg::JsonBody {
+              arg: ident.clone(),
+              span: ident.span()
+            })
+          }
+          else if let Some(_query_att) = get_att(attrs, "query") {
+            Some(HttpArg::Query {
+              name: LitStr::new(ident.to_string().as_str(), ident.span()),
+              arg: ident.clone(),
+              span: ident.span()
+            })
+          }
+          else {
+            Some(HttpArg::Uri {
+              arg: ident.clone(),
+              span: ident.span(),
+            })
+          }
+        } else {
+          None
+        }
+      }
+      _ => None,
+    }
+  }
 }
 
 fn build_method(
-    attrs: &mut Vec<Attribute>,
-    sig: &mut Signature,
+  attrs: &mut Vec<Attribute>,
+  sig: &mut Signature,
 ) -> Result<ImplItemMethod, syn::Error> {
-    let attr = get_att(attrs, "get", sig.span())?;
-    let att = HttpMethodAttribute::try_from(attr)?;
+  let attr = get_att(attrs, "http").ok_or(syn::Error::new(sig.span(), "Missing http method attribute"))?;
+  let att = HttpMethodAttribute::try_from(attr)?;
 
-    let uri_format_args = sig.inputs.iter().filter_map(|a| match a {
-        FnArg::Typed(PatType { pat, .. }) => {
-            if let Pat::Ident(ident) = pat.as_ref() {
-                Some(quote_spanned!(ident.span()=>#ident = #ident))
-            } else {
-                None
-            }
-        }
-        _ => None,
-    });
+  let args = sig.inputs.iter_mut()
+    .filter_map(HttpArg::parse)
+    .collect::<Vec<_>>();
 
-    let uri = att.uri;
-    let method = att.method;
+  let uri_args = args.iter().filter_map(|a| match a {
+    HttpArg::Uri {
+      arg,
+      span
+    } => Some(quote_spanned!(*span=> #arg = #arg)),
+    _ => None
+  });
 
-    let uri = quote_spanned!(uri.span()=>concat!("{}", #uri));
+  let query_args = args.iter().filter_map(|a| match a {
+    HttpArg::Query {
+      name,
+      arg,
+      span
+    } => Some(quote_spanned!(*span=> (#name, #arg))),
+    _ => None
+  }).collect::<Vec<_>>();
 
-    Ok(parse_quote! {
+  let query = if query_args.is_empty() {None} else {
+    Some(quote! { .query(&[#(#query_args)*]) })
+  };
+
+  let body_args = args.iter().filter_map(|a| match a {
+    HttpArg::JsonBody {
+      arg,
+      span
+    } => Some(quote_spanned!(*span=> .json(#arg))),
+    _ => None
+  });
+
+  let uri = att.uri;
+  let method = att.method;
+
+  let uri = quote_spanned!(uri.span()=>concat!("{}", #uri));
+
+  Ok(parse_quote! {
       #(#attrs)*
       #sig {
-       Ok(self.client.#method(format!(#uri, self.endpoint#(, #uri_format_args)*))
+       Ok(self.client.#method(format!(#uri, self.endpoint#(, #uri_args)*))
+          #query
+          #(#body_args)*
           .send().await.map_err(retroqwest::RetroqwestError::RequestError)?
           .error_for_status().map_err(retroqwest::RetroqwestError::ResponseError)?
           .json().await.map_err(retroqwest::RetroqwestError::JsonParse)?)
@@ -95,34 +166,34 @@ fn build_method(
 }
 
 fn expand(mut def: ItemTrait) -> Result<proc_macro2::TokenStream, syn::Error> {
-    let trait_name = &def.ident;
-    let name = Ident::new(&format!("{}Client", trait_name), def.ident.span());
-    let vis = &def.vis;
+  let trait_name = &def.ident;
+  let name = Ident::new(&format!("{}Client", trait_name), def.ident.span());
+  let vis = &def.vis;
 
-    let mut methods: Vec<ImplItemMethod> = vec![];
+  let mut methods: Vec<ImplItemMethod> = vec![];
 
-    for x in &mut def.items {
-        match x {
-            TraitItem::Method(TraitItemMethod {
-                attrs,
-                sig,
-                default,
-                ..
-            }) => {
-                if default.is_some() {
-                    return Err(syn::Error::new(
-                        default.as_ref().unwrap().span(),
-                        "retroquest trait methods cannot have defaults",
-                    ));
-                }
-
-                methods.push(build_method(attrs, sig)?)
-            }
-            _ => (),
+  for x in &mut def.items {
+    match x {
+      TraitItem::Method(TraitItemMethod {
+                          attrs,
+                          sig,
+                          default,
+                          ..
+                        }) => {
+        if default.is_some() {
+          return Err(syn::Error::new(
+            default.as_ref().unwrap().span(),
+            "retroquest trait methods cannot have defaults",
+          ));
         }
-    }
 
-    let client = quote! {
+        methods.push(build_method(attrs, sig)?)
+      }
+      _ => (),
+    }
+  }
+
+  let client = quote! {
       #[derive(Clone, Debug)]
       #vis struct #name {
         endpoint: String,
@@ -147,9 +218,9 @@ fn expand(mut def: ItemTrait) -> Result<proc_macro2::TokenStream, syn::Error> {
       }
     };
 
-    def.attrs.push(parse_quote!(#[async_trait::async_trait]));
+  def.attrs.push(parse_quote!(#[async_trait::async_trait]));
 
-    Ok(quote! {
+  Ok(quote! {
       #def
 
       #client
--- tests/test.rs
+++ tests/test.rs
@@ -3,97 +3,129 @@ use retroqwest::RetroqwestError;
 
 use serde::{Deserialize, Serialize};
 use wiremock::{
-    matchers::{method, path},
-    Mock, ResponseTemplate,
+  matchers::{method, path},
+  Mock, ResponseTemplate,
 };
+use wiremock::matchers::{query_param, body_json};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct HttpBinResponse {
-    pub url: String,
+  pub url: String,
 }
 
 #[retroqwest::retroqwest]
 pub trait HttpBin {
-    #[get::json("/anything")]
-    async fn get_anything(&self) -> Result<HttpBinResponse, RetroqwestError>;
+  #[http::get("/anything")]
+  async fn get_anything(&self) -> Result<HttpBinResponse, RetroqwestError>;
 
-    #[get::json("/anything/{name}")]
-    async fn get_by_name(&self, name: String) -> Result<HttpBinResponse, RetroqwestError>;
+  #[http::get("/anything/{name}")]
+  async fn get_by_name(&self, name: String) -> Result<HttpBinResponse, RetroqwestError>;
+
+  #[http::post("/anything/{name}")]
+  async fn post_to_name(&self, name: String, #[query] q: bool, #[json] body: &HttpBinResponse) -> Result<HttpBinResponse, RetroqwestError>;
 }
 
 impl HttpBinClient {
-    pub fn new(base_uri: String) -> Result<Self, RetroqwestError> {
-        Self::from_builder(base_uri, ClientBuilder::default())
-    }
+  pub fn new(base_uri: String) -> Result<Self, RetroqwestError> {
+    Self::from_builder(base_uri, ClientBuilder::default())
+  }
 }
 
 // This method allows for better code completion
 // since `impl HttpBin` is better than the generated struct...
 fn build_client(uri: String) -> Result<impl HttpBin, retroqwest::RetroqwestError> {
-    Ok(HttpBinClient::new(uri)?)
+  Ok(HttpBinClient::new(uri)?)
 }
 
 #[tokio::test]
 async fn test_simple_gets() -> Result<(), Box<dyn std::error::Error>> {
-    let server = wiremock::MockServer::start().await;
+  let server = wiremock::MockServer::start().await;
 
-    Mock::given(method("GET"))
-        .and(path("/anything"))
-        .respond_with(ResponseTemplate::new(200).set_body_json(HttpBinResponse {
-            url: "test".to_string(),
-        }))
-        .mount(&server)
-        .await;
+  Mock::given(method("GET"))
+    .and(path("/anything"))
+    .respond_with(ResponseTemplate::new(200).set_body_json(HttpBinResponse {
+      url: "test".to_string(),
+    }))
+    .mount(&server)
+    .await;
 
-    let client = build_client(server.uri())?;
-    let result: HttpBinResponse = client.get_anything().await?;
+  let client = build_client(server.uri())?;
+  let result: HttpBinResponse = client.get_anything().await?;
 
-    assert_eq!(result.url, "test".to_string());
-    Ok(())
+  assert_eq!(result.url, "test".to_string());
+  Ok(())
 }
 
 #[tokio::test]
 async fn test_gets_with_vars_in_path() -> Result<(), Box<dyn std::error::Error>> {
-    let server = wiremock::MockServer::start().await;
+  let server = wiremock::MockServer::start().await;
 
-    Mock::given(method("GET"))
-        .and(path("/anything/test"))
-        .respond_with(ResponseTemplate::new(200).set_body_json(HttpBinResponse {
-            url: "test".to_string(),
-        }))
-        .mount(&server)
-        .await;
+  Mock::given(method("GET"))
+    .and(path("/anything/test"))
+    .respond_with(ResponseTemplate::new(200).set_body_json(HttpBinResponse {
+      url: "test".to_string(),
+    }))
+    .mount(&server)
+    .await;
 
-    let client = build_client(server.uri())?;
+  let client = build_client(server.uri())?;
 
-    let result: HttpBinResponse = client.get_by_name("test".to_string()).await?;
+  let result: HttpBinResponse = client.get_by_name("test".to_string()).await?;
 
-    assert_eq!(result.url, "test".to_string());
+  assert_eq!(result.url, "test".to_string());
 
-    Ok(())
+  Ok(())
+}
+
+#[tokio::test]
+async fn test_complex_post() -> Result<(), Box<dyn std::error::Error>> {
+  let server = wiremock::MockServer::start().await;
+
+  let body = HttpBinResponse {
+    url: "test".to_string(),
+  };
+
+  Mock::given(method("POST"))
+    .and(path("/anything/test"))
+    .and(query_param("q", "true"))
+    .and(body_json(body.clone()))
+    .respond_with(ResponseTemplate::new(200).set_body_json(HttpBinResponse {
+      url: "posted".to_string()
+    }))
+    .mount(&server)
+    .await;
+
+  let client = build_client(server.uri())?;
+
+  let result: HttpBinResponse = client.post_to_name("test".to_string(), true, &body).await?;
+
+  assert_eq!(result.url, "posted".to_string());
+
+  Ok(())
 }
 
 #[tokio::test]
 async fn test_get_errors() -> Result<(), Box<dyn std::error::Error>> {
-    let server = wiremock::MockServer::start().await;
+  let server = wiremock::MockServer::start().await;
 
-    Mock::given(method("GET"))
-        .and(path("/anything/test"))
-        .respond_with(ResponseTemplate::new(400))
-        .mount(&server)
-        .await;
+  Mock::given(method("GET"))
+    .and(path("/anything/test"))
+    .respond_with(ResponseTemplate::new(400))
+    .mount(&server)
+    .await;
 
-    let client = build_client(server.uri())?;
+  let client = build_client(server.uri())?;
 
-    let result: String = client
-        .get_by_name("test".to_string())
-        .await
-        .err()
-        .unwrap()
-        .to_string();
+  let result: String = client
+    .get_by_name("test".to_string())
+    .await
+    .err()
+    .unwrap()
+    .to_string();
 
-    assert!(result.starts_with(
-        "Response status code indicates error: HTTP status client error (400 Bad Request)"
-    ));
-    Ok(())
+  assert!(result.starts_with(
+    "Response status code indicates error: HTTP status client error (400 Bad Request)"
+  ));
+
+  Ok(())
 }
